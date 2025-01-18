86124a4dfb1945d0f2784016816d85987e202397
VinodiNikeshaniKasthuri <vinodinikeshani@gmail.com>
2025-01-18 18:37:47 +0530

    complete navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -35,11 +35,11 @@ const Navbar = () => {
           ? <div className='flex items-center gap-2 cursor-pointer group relative'>
             <img className='w-8 rounded-full'src={assets.profile_pic} alt="profile_pic" />
             <img className='w-2.5'src={assets.dropdown_icon} alt="dropdown_icon" />
-            <div>
-              <div>
-                <p>Mt profile</p>
-                <p>My Appointment</p>
-                <p>Logout</p>
+            <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
+              <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+                <p onClick={()=>navigate('my-profile')} className='hover:text-black cursor-pointer'>Mt profile</p>
+                <p onClick={()=>navigate('my-appoinments')} className='hover:text-black cursor-pointer'>My Appointment</p>
+                 <p onClick={()=>setToken(false)} className='hover:text-black cursor-pointer'>Logout</p>
 
                 
               </div>
