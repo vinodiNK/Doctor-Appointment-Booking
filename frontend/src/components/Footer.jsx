e844e1d38f3acd1ce212332f445b7b07117d9a7e
VinodiNikeshaniKasthuri <vinodinikeshani@gmail.com>
2025-01-22 23:45:35 +0530

    Complete home page

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,27 +3,27 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div>
-      <div>
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
         {/*---- Left side------*/}
         <div>
-           <img src={assets.logo} alt="" />
-           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+           <img className='mb-5 w-40' src={assets.logo} alt="" />
+           <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
         </div>
         {/*-----Center Section----*/}
         <div>
-          <p>Company</p>
-          <ui>
+          <p className='text-xl font-medium mb-5'>Company</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
             <li>Home</li>
             <li>About</li>
             <li>Contact us</li>
             <li>Privacy Policy</li>
-          </ui>
+          </ul>
         </div>
         {/*---- Right Section ---- */}
         <div>
-           <p>GET IN TOUCH</p>
-           <ul>
+           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+           <ul  className='flex flex-col gap-2 text-gray-600'>
             <li>0788548741</li>
             <li>doctor@gmail.com</li>
            </ul>
@@ -32,7 +32,7 @@ const Footer = () => {
       {/* --------- Copyright Text -------*/}
       <div>
         <hr />
-        <p>Copyright © 2024 GreatStack - All Right Reserved.</p>
+        <p className='py-5 text-sm text-center'>Copyright © 2024 GreatStack - All Right Reserved.</p>
       </div>
     </div>
   )
