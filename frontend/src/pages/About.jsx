2651b7b7af602dd2b8cd318c32cbcf489e08e31b
VinodiNikeshaniKasthuri <vinodinikeshani@gmail.com>
2025-01-23 23:58:01 +0530

    Complete About page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const About = () => {
   return (
     <div>
-      <p>ABOUT <span> US</span></p>
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+      <p>ABOUT <span className='text-gray-700 font-medium'> US</span></p>
+    </div>
+
+    <div className='my-10 flex flex-col md:flex-row gap-12'>
+      <img className='w-full md:max-w-[360px]' src={assets.about_image} alt="" />
+      <div>
+        <p>Welcome to Prescripto, your trusted partner in managing your healthcare needs conveniently and efficiently. At Prescripto, we understand the challenges individuals face when it comes to scheduling doctor appointments and managing their health records.</p>
+        <p>Prescripto is committed to excellence in healthcare technology. We continuously strive to enhance our platform, integrating the latest advancements to improve user experience and deliver superior service. Whether you're booking your first appointment or managing ongoing care, Prescripto is here to support you every step of the way.</p>
+        <b>Our Vision</b>
+        <p>Our vision at Prescripto is to create a seamless healthcare experience for every user. We aim to bridge the gap between patients and healthcare providers, making it easier for you to access the care you need, when you need it.</p>
+      </div>
+    </div>
+
     </div>
   )
 }
