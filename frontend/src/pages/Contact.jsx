22a1e8c201ec181f85bc42a12ffc5472073a7396
VinodiNikeshaniKasthuri <vinodinikeshani@gmail.com>
2025-01-24 00:46:51 +0530

    Create contact page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-      <h1>hi</h1>
-    </div>
+     <div className='text-center text-2xl pt-10 text-gray-500'></div>
+     <p>CONTACT <span className='text-gray-700 font-semibold'>US</span></p>
+      </div>
   )
 }
 
