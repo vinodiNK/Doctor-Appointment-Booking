c0b3a4329506b858d6208989772181162a397f1d
VinodiNikeshaniKasthuri <vinodinikeshani@gmail.com>
2025-01-25 23:50:54 +0530

    Update MyAppointment file

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -9,21 +9,21 @@ const MyAppointments = () => {
     <p className='pb-3 mt-12 font-medium text-zinc-700 border-b'>My Appointments</p>
     <div>
       {doctors.slice(0,2).map((item,index)=>(
-        <div key={index}>
+        <div className='grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b' key={index}>
           <div>
-            <img src={item.image} />
+            <img className='w-32 bg-indigo-50' src={item.image} />
           </div>
           <div>
-          <p>{item.name}</p>
-          <p>{item.speciality}</p>
-          <p>Address</p>
-          <p>{item.address.line1}</p>
-          <p>{item.address.line2}</p>
-          <p><span>Date & Time</span> 25, January, 2025 | 11.20 PM</p>
+          <p className='flex-1 text-sm text-zinc-600'>{item.name}</p>
+          <p >{item.speciality}</p>
+          <p className='text-zinc-700 font-medium mt-1'>Address</p>
+          <p className='text-xs'>{item.address.line1}</p>
+          <p className='text-xs'>{item.address.line2}</p>
+          <p className='text-xm mt-1'><span className='text-sm text-neutral-700 font-medium'>Date & Time</span> 25, January, 2025 | 11.20 PM</p>
         </div>
         <div></div>
-        <div>
-          <button>Pay Online</button>
+        <div className='flex flex-col gap-2 justify-end'>
+          <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded'>Pay Online</button>
           <button>Cancel Appointment</button>
         </div>
         </div>
