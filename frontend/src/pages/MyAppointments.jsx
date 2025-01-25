cfd4b53f2b6e14a12439acb178db91f1b8bc5c39
VinodiNikeshaniKasthuri <vinodinikeshani@gmail.com>
2025-01-25 23:57:49 +0530

    Complete MyAppointment file

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -8,7 +8,7 @@ const MyAppointments = () => {
     <div>
     <p className='pb-3 mt-12 font-medium text-zinc-700 border-b'>My Appointments</p>
     <div>
-      {doctors.slice(0,2).map((item,index)=>(
+      {doctors.slice(0,3).map((item,index)=>(
         <div className='grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b' key={index}>
           <div>
             <img className='w-32 bg-indigo-50' src={item.image} />
@@ -23,8 +23,8 @@ const MyAppointments = () => {
         </div>
         <div></div>
         <div className='flex flex-col gap-2 justify-end'>
-          <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded'>Pay Online</button>
-          <button>Cancel Appointment</button>
+          <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-primary hover:text-white transition-all duration-300'>Pay Online</button>
+          <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded  hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel Appointment</button>
         </div>
         </div>
       ))}
