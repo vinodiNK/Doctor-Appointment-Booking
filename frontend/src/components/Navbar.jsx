74aa6e91f5240949d2558e84d06001e1ceb54074
VinodiNikeshaniKasthuri <vinodinikeshani@gmail.com>
2025-01-26 22:22:30 +0530

    Change the navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -13,19 +13,19 @@ const Navbar = () => {
       <img onClick={()=>navigate('/')} className='w-44 cursor-pointer' src={assets.admin_logo} alt="" />
       <ul className='hidden md:flex items-start gap-5 font-medium'>
         <NavLink to='/'>
-            <li className='py-1'>HOME</li>
+            <li className='py-1'>Home</li>
             <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
         </NavLink>
         <NavLink to='/doctors'>
-            <li className='py-1'>ALL DOCTORS</li>
+            <li className='py-1'>All Doctors</li>
             <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
         </NavLink>
         <NavLink to='/about' >
-            <li className='py-1'>ABOUT</li>
+            <li className='py-1'>About</li>
             <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
         </NavLink>
         <NavLink to='/contact'>
-            <li className='py-1'>CONTACT</li>
+            <li className='py-1'>Contact</li>
             <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
         </NavLink>
       </ul>
