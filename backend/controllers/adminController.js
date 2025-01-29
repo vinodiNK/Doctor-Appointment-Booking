85b690ea84a98c6d4f1076548119a068d9911f02
VinodiNikeshaniKasthuri <vinodinikeshani@gmail.com>
2025-01-29 10:00:13 +0530

    create adminController

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,12 @@
+// API for adding doctor
+const addDoctor = async(requestAnimationFrame, res) => {
+
+    try {
+        const { name, email, password, speciality, degree, experience, about, fees, address } = req.body
+
+    } catch (error) {
+
+    }
+}
+
+export { addDoctor }
