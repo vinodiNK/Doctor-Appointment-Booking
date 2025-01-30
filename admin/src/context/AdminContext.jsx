932ebc6ea9e555fe0cc4be99048bc8976e5b803d
VinodiNikeshaniKasthuri <vinodinikeshani@gmail.com>
2025-01-30 23:53:25 +0530

    Login Page Changes

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,10 +1,18 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const AdminContext = createContext()
 
 const AdminContextProvider = (props) =>{
 
+    const [aToken,setAToken] = useState('')
+
+    const backendUrl = import.meta.env.VITE_BACKEND_URL 
+
+
+
     const value = {
+        aToken,setAToken,
+        backendUrl,
 
     }
 
