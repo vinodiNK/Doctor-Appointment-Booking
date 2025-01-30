c7b25122a5b4adec3b446687fa1de279ea223c58
VinodiNikeshaniKasthuri <vinodinikeshani@gmail.com>
2025-01-31 01:32:15 +0530

    Add Navbar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,22 +1,27 @@
 import React, { useContext } from 'react';
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
+import Navbar from './components/Navbar';
 import { AdminContext } from './context/AdminContext';
 import Login from './pages/Login';
 
 
 const App = () => {
 
-  const {aToken} = useContext(AdminContext)
+  const { aToken } = useContext(AdminContext)
 
   return aToken ? (
-    <div >
+    <div className='bg-[#F8F9FD]' >
     
       <ToastContainer />
+      
+      <Login />
       </div>
+
   ) : (
     <>
-    <Login />
+    <Navbar />
+    
     <ToastContainer />
     </>
   )
