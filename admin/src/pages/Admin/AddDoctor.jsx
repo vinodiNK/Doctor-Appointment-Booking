7fd95bab04934a17c1e1294de0aafdc812cadd78
VinodiNikeshaniKasthuri <vinodinikeshani@gmail.com>
2025-02-02 23:58:06 +0530

    create add doctor

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -1,10 +1,95 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const AddDoctor = () => {
   return (
-    <div>
-      AddDoctors
-    </div>
+    <form>
+        <p>Add Doctor</p>
+
+        <div>
+            <div>
+                <label htmlFor="doc-img">
+                    <img src={assets.upload_area} alt="" />
+                </label>
+                <input type="file" id="doc-img" hidden/>
+                <p>Upload doctor <br /> picture</p>
+            </div>
+
+            <div>
+                <div>
+                    <div>
+                        <p> Doctor name</p>
+                        <input type='text' placeholder='Name' required/>
+                    </div>
+
+                    <div>
+                        <p> Doctor email</p>
+                        <input type='email' placeholder='Email' required/>
+                    </div>
+
+                    <div>
+                        <p> Doctor Password</p>
+                        <input type='password' placeholder='Password' required/>
+                    </div>
+
+                    <div>
+                        <p>Experience</p>
+                        <select name="" id="">
+                            <option value="1 Year">1 Year</option>
+                            <option value="2 Year">2 Year</option>
+                            <option value="3 Year">3 Year</option>
+                            <option value="4 Year">4 Year</option>
+                            <option value="5 Year">5 Year</option>
+                            <option value="6 Year">6 Year</option>
+                            <option value="7 Year">7 Year</option>
+                            <option value="8 Year">8 Year</option>
+                            <option value="9 Year">9 Year</option>
+                            <option value="10 Year">10 Year</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <p> Fees</p>
+                        <input type='number' placeholder='fees' required/>
+                    </div>
+                         
+                         <div>
+                            <div>
+                                <p>Speciality</p>
+                                <select name='' id="">
+                                    <option value="General physician"> General physician </option>
+                                    <option value="Gynecologist">Gynecologist</option>
+                                    <option value="Dermatologist">Dermatologist</option>
+                                    <option value="Pediatricians">Pediatricians</option>
+                                    <option value="Neurologist">Neurologist</option>
+                                    <option value="Gastroenterologist">Gastroenterologist</option>
+
+                                 </select>
+                            </div>
+                            <div>
+                                <p>Education</p>
+                                <input type="text" placeholder='Education' required/>
+                            </div>
+
+                            <div>
+                                <p>Address</p>
+                                <input type="text" placeholder='address1' required/>
+                                <input type="text" placeholder='address2' required/>
+                            </div>
+                                 
+                            <div>
+                                <p>About Doctor</p>
+                                <textarea  placeholder='write about doctor' rows={5} required/>
+                            </div>
+                            
+                            <button>Add Doctor</button>
+                            
+                         </div>
+
+                </div>
+            </div>
+        </div>
+    </form>
   )
 }
 
