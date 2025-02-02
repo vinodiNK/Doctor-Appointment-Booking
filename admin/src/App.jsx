831fdcde794d46cc7d998b56bd887cd17ae82e38
VinodiNikeshaniKasthuri <vinodinikeshani@gmail.com>
2025-02-02 21:40:32 +0530

    Create side bar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,14 @@
 import React, { useContext } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import { AdminContext } from './context/AdminContext';
+import AddDoctor from './pages/Admin/AddDoctor';
+import AllApointments from './pages/Admin/AllApointments';
+import Dashboard from './pages/Admin/Dashboard';
+import DoctorsList from './pages/Admin/DoctorsList';
 import Login from './pages/Login';
 
 
@@ -13,11 +18,19 @@ const App = () => {
 
   return aToken ? (
     <div className='bg-[#F8F9FD]' >
-    
+      
       <ToastContainer />
       <Navbar />
+      
       <div className='flex items-start'>
         <Sidebar />
+        <Routes>
+          <Route path ='/' element={<></>} />
+          <Route path ='/admin-dashboard' element={<Dashboard/>} />
+          <Route path ='/all-appointments' element={<AllApointments/>} />
+          <Route path ='/add-doctor' element={<AddDoctor/>} />
+          <Route path ='/doctor-list' element={<DoctorsList/>} />
+        </Routes>
       </div>
       
       </div>
--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { NavLink } from 'react-router-dom'
+import { assets } from '../assets/assets'
+import { AdminContext } from '../context/AdminContext'
 
 const Sidebar = () => {
+
+const {aToken} = useContext(AdminContext)
+
   return (
     <div>
-      
+      {
+        aToken && <ul>
+          <NavLink to ={'/admin-dashboard'}>
+            <img src={assets.home_icon} alt="" />
+            <p>Dashboard</p>
+          </NavLink>
+
+          <NavLink to={'all-appointments'}>
+            <img src={assets.appointment_icon} alt="" />
+            <p>Appointments</p>
+          </NavLink>
+
+          <NavLink to={'/add-doctor'}>
+            <img src={assets.add_icon} alt="" />
+            <p>Add Doctor</p>
+          </NavLink>
+
+          <NavLink to={'/doctors-list'}>
+            <img src={assets.people_icon} alt="" />
+            <p>Doctors List</p>
+          </NavLink>
+
+        </ul>
+      }
     </div>
   )
 }
