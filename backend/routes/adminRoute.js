cbc17f42b38e4a3164130686afb0e12211dca6f1
VinodiNikeshaniKasthuri <vinodinikeshani@gmail.com>
2025-02-14 14:31:08 +0530

    Complete Admin Doctors List

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { addDoctor, allDoctors, loginAdmin } from '../controllers/adminController.js'
+import { changeAvailability } from '../controllers/doctorController.js'
 import authAdmin from '../middlewares/authAdmin.js'
 import upload from '../middlewares/multer.js'
 
@@ -9,5 +10,6 @@ const adminRouter = express.Router()
 adminRouter.post('/add-doctor', authAdmin, upload.single('image'), addDoctor)
 adminRouter.post('/login', loginAdmin)
 adminRouter.post('/all-doctors', authAdmin, allDoctors)
+adminRouter.post('/change-availability', authAdmin, changeAvailability)
 
 export default adminRouter
